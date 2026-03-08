f743ef0d88c7495082979acd3548c28e3adf234a
Cabrel-Douanla <cabrelniamekon@gmail.com>
2026-03-08 09:47:13 +0100

    fix(auth): refreshing token on 403

--- lib/store/services/api.ts
+++ lib/store/services/api.ts
@@ -20,7 +20,7 @@ const baseQueryWithReauth: BaseQueryFn<
 > = async (args, api, extraOptions) => {
   let result = await baseQuery(args, api, extraOptions);
 
-  if (result.error && result.error.status === 401) {
+  if (result.error && result.error.status === 403) {
     const refreshToken = tokenStore.getRefreshToken();
     if (refreshToken) {
       // try to get a new token
